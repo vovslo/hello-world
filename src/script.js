c854e02d47e1978dcca7991503a07ec4551d24e3
Vladymir Slobodyan <vovslo.ua@gmail.com>
2019-04-05 18:40:01 +0300

    Change js

--- src/script.js
+++ src/script.js
@@ -1,29 +1,13 @@
-let sum = (10, 5 - 1) * 2;
-
-alert(sum);
-/*
-alert(2 && 1 && null && 0 && undefined);
-function F() {}
-alert(F() instanceof);
-
-function F() { return F; }
-
-alert( new F() instanceof F );
-alert( new F() instanceof Function );
-
-alert(str); // ?
-let str = "Hello";
 
 sayHi();
 
 function sayHi() {
   alert("Hello");
 }
-alert(0 || 1 && 2 || 3);
 
 function hello () {
 	alert("Hello World!!!");
 }
+
 hello();
 document.write('Hello World!!!');
-*/
